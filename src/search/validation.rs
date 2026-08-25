8b2ad3a55db47ed3b72352756db26a1477bd086a
Arturo Hernández <arturoaureliohr@gmail.com>
2026-08-24 22:48:44 -0400

    test(search): add search condition validation tests

--- src/search/validation.rs
+++ src/search/validation.rs
@@ -29,3 +29,134 @@ impl SearchConditionStatement {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_validate_valid_search_condition_group_with_one_condition() {
+        let mut search_condition_group = SearchConditionGroup::default();
+
+        search_condition_group
+            .conditions
+            .push(SearchCondition::Statement(
+                SearchConditionStatement::HasTag { tag_id: Some(1) },
+            ));
+
+        assert!(search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_valid_search_condition_group_with_multiple_conditions() {
+        let mut search_condition_group = SearchConditionGroup::default();
+
+        search_condition_group.conditions.extend([
+            SearchCondition::Statement(SearchConditionStatement::HasTag { tag_id: Some(1) }),
+            SearchCondition::Statement(SearchConditionStatement::DoesNotHaveTag {
+                tag_id: Some(3),
+            }),
+        ]);
+
+        assert!(search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_valid_search_condition_group_with_nested_conditions() {
+        let mut search_condition_group = SearchConditionGroup::default();
+
+        let mut nested_search_condition_group = SearchConditionGroup::default();
+
+        nested_search_condition_group.conditions.extend([
+            SearchCondition::Statement(SearchConditionStatement::HasTag { tag_id: Some(1) }),
+            SearchCondition::Statement(SearchConditionStatement::DoesNotHaveTag {
+                tag_id: Some(3),
+            }),
+        ]);
+
+        search_condition_group
+            .conditions
+            .extend([SearchCondition::Group(nested_search_condition_group)]);
+
+        assert!(search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_invalid_search_condition_group_with_no_conditions() {
+        let search_condition_group = SearchConditionGroup::default();
+
+        assert!(!search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_invalid_search_condition_group_with_one_invalid_condition() {
+        let mut search_condition_group = SearchConditionGroup::default();
+
+        search_condition_group
+            .conditions
+            .push(SearchCondition::Statement(
+                SearchConditionStatement::HasTag { tag_id: None },
+            ));
+
+        assert!(!search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_invalid_search_condition_group_with_multiple_conditions() {
+        let mut search_condition_group = SearchConditionGroup::default();
+
+        search_condition_group.conditions.extend([
+            SearchCondition::Statement(SearchConditionStatement::HasTag { tag_id: Some(1) }),
+            SearchCondition::Statement(SearchConditionStatement::DoesNotHaveTag { tag_id: None }),
+        ]);
+
+        assert!(!search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_invalid_search_condition_group_with_nested_conditions() {
+        let mut search_condition_group = SearchConditionGroup::default();
+
+        let mut nested_search_condition_group = SearchConditionGroup::default();
+
+        nested_search_condition_group.conditions.extend([
+            SearchCondition::Statement(SearchConditionStatement::HasTag { tag_id: Some(1) }),
+            SearchCondition::Statement(SearchConditionStatement::DoesNotHaveTag { tag_id: None }),
+        ]);
+
+        search_condition_group
+            .conditions
+            .extend([SearchCondition::Group(nested_search_condition_group)]);
+
+        assert!(!search_condition_group.validate());
+    }
+
+    #[test]
+    fn should_validate_valid_has_tag_search_condition_statement() {
+        let search_condition_statement = SearchConditionStatement::HasTag { tag_id: Some(1) };
+
+        assert!(search_condition_statement.validate());
+    }
+
+    #[test]
+    fn should_validate_invalid_has_tag_search_condition_statement() {
+        let search_condition_statement = SearchConditionStatement::HasTag { tag_id: None };
+
+        assert!(!search_condition_statement.validate());
+    }
+
+    #[test]
+    fn should_validate_valid_does_not_have_tag_search_condition_statement() {
+        let search_condition_statement =
+            SearchConditionStatement::DoesNotHaveTag { tag_id: Some(1) };
+
+        assert!(search_condition_statement.validate());
+    }
+
+    #[test]
+    fn should_validate_invalid_does_not_have_tag_search_condition_statement() {
+        let search_condition_statement = SearchConditionStatement::DoesNotHaveTag { tag_id: None };
+
+        assert!(!search_condition_statement.validate());
+    }
+}
